1ab9a9ff71d1895f7be8800e1794254615122625
denismq <denisitoal25@gmail.com>
2023-08-11 14:36:30 -0500

    added header and sections

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <section className='section section--intro'>
+            <div className="o-container">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6">1</div>
+                        <div className="col-md-6">form</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section className='section section--discount'>
+            <div className="o-container"></div>
+        </section>
+        <section className='section section--technology'>
+            <div className="o-container"></div>
+        </section>
+        
+    </>
+  )
+}
+
+export default Home
